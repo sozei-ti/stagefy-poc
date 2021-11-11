988fe0197081d7f40e71e0ae595cdae7f4959770
ebertmota <ebert.mota03@gmail.com>
2021-11-11 11:40:27 -0300

    chore: setup app

---
+++ pages/Home/index.tsx
@@ -0,0 +1,116 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {useEffect, useState} from 'react';
+import {View, Text, TouchableOpacity, ScrollView} from 'react-native';
+import RtcEngine, {
+  RtcLocalView,
+  RtcRemoteView,
+  VideoRenderMode,
+} from 'react-native-agora';
+import {requestCameraAndAudioPermission} from '../utils/permissions';
+import {styles} from './styles';
+export const Home: React.FC = () => {
+  const [peerIds, setPeerIds] = useState([] as number[]);
+  const [isJoinSuceed, setIsJoinSuceed] = useState(false);
+  const [engine, setEngine] = useState({} as RtcEngine);
+
+  const appId = 'e5c64fc6afda478996fc412a4b61aed5';
+  const token =
+    '006e5c64fc6afda478996fc412a4b61aed5IABY/m5mud609vVLc/gmW2zdrdYaHTMRJIXVh6jVyRJ4swx+f9gAAAAAEAACwxdSCnKOYQEAAQAKco5h';
+  const channelName = 'test';
+
+  const initializeAgora = async () => {
+    const newEngine = await RtcEngine.create(appId);
+    setEngine(newEngine);
+    await newEngine.enableVideo();
+
+    newEngine.addListener('UserJoined', (userId, elapsed) => {
+      console.log('UserJoined', userId, elapsed);
+
+      if (peerIds.indexOf(userId) === -1) {
+        setPeerIds(state => [...state, userId]);
+      }
+    });
+
+    newEngine.addListener('UserOffline', (userId, elapsed) => {
+      console.log('UserOffline', userId, elapsed);
+
+      setPeerIds(state => state.filter(item => item !== userId));
+    });
+
+    newEngine.addListener('JoinChannelSuccess', (channel, userId, elapsed) => {
+      console.log('JoinChannelSuccess', channel, userId, elapsed);
+
+      setIsJoinSuceed(true);
+    });
+  };
+
+  const handleStartCall = async () => {
+    await engine.joinChannel(token, channelName, null, 0);
+  };
+
+  const handleEndCall = async () => {
+    await engine.leaveChannel();
+    setIsJoinSuceed(false);
+    setPeerIds([]);
+  };
+
+  const renderRemoteVideos = () => {
+    return (
+      <ScrollView
+        style={styles.remoteContainer}
+        contentContainerStyle={{paddingHorizontal: 2.5}}
+        horizontal={true}>
+        {peerIds.map((value, index, array) => {
+          return (
+            <RtcRemoteView.SurfaceView
+              style={styles.remote}
+              uid={value}
+              channelId={channelName}
+              renderMode={VideoRenderMode.Hidden}
+              zOrderMediaOverlay={true}
+            />
+          );
+        })}
+      </ScrollView>
+    );
+  };
+
+  const renderVideos = () => {
+    if (isJoinSuceed) {
+      return (
+        <View style={styles.fullView}>
+          <RtcLocalView.SurfaceView
+            style={styles.max}
+            channelId={channelName}
+            renderMode={VideoRenderMode.Hidden}
+          />
+          {renderRemoteVideos()}
+        </View>
+      );
+    }
+
+    return <></>;
+  };
+
+  useEffect(() => {
+    // requestCameraAndAudioPermission();
+    initializeAgora();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <View style={styles.max}>
+      <View style={styles.max}>
+        <View style={styles.buttonHolder}>
+          <TouchableOpacity onPress={handleStartCall} style={styles.button}>
+            <Text style={styles.buttonText}> Start Call </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleEndCall} style={styles.button}>
+            <Text style={styles.buttonText}> End Call </Text>
+          </TouchableOpacity>
+        </View>
+        {renderVideos()}
+      </View>
+    </View>
+  );
+};
